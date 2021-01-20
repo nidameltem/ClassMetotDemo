609d2789b8d48198e2af973c3acade4620c4d8d0
nidameltem95@gmail.com <nidameltem95@gmail.com>
2021-01-20 17:16:05 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void Ekle(Musteri must)
+        {
+            Console.WriteLine("Müşteri Bilgileri Eklendi : " + must.Ad + must.Soyad);
+
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Meltem";
+            musteri1.Soyad = "Ağın";
+            musteri1.Id = 152;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Eda";
+            musteri2.Soyad = "Kaya";
+            musteri2.Id = 153;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri must in musteriler)
+            {
+                Console.WriteLine(must.Ad);
+                Console.WriteLine(must.Soyad);
+                Console.WriteLine(must.Id);
+                Console.WriteLine("**************************************");
+
+            }
+            Console.WriteLine("**************************************");
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+        }
+    }
+}
